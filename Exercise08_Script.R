de81faee7aa5cb7ef0476de8f5e3de140a113a73
Kyle Walker <kwalke11@nd.edu>
2020-10-22 17:02:31 -0400

    Problem 1 finished

--- Exercise08_Script.R
+++ Exercise08_Script.R
@@ -1,15 +1,10 @@
 setwd("~/Desktop/Biocomputing_R/Biocomputing2020_Exercise08") #set working directory
 scores <- read.table(file="UWvMSU_1-22-13.txt",header=TRUE, sep='') #read scores file and create table with scoring data
 cm_scores <- data.frame("time"=(scores$time),"team1"="UW", "team2"="MSU")
-UW_scores <- data.frame("time"=scores$time[scores$team=="UW"], "UW_Score"=cumsum(scores$score[scores$team=="UW"]), "MSU_Score")
-for (i in 1:nrow(scores)){
-  if (scores$team[i]=="UW")
-    cm_scores$UW_Score <- cumsum(scores$score[i]){
-      }else{
-        cm_scores$MSU_Score <- cumsum(scores$score[i])
-      }
-}
+UW_scores <- data.frame("time"=scores$time[scores$team=="UW"], "MSU_Score"=0,UW_Score=cumsum(scores$score[scores$team=="UW"]))
 UW_scores
-MSU_scores <- data.frame("time"=scores$time[scores$team=="MSU"], "MSU_Score"=cumsum(scores$score[scores$team=="MSU"]), "UW_Score")
+MSU_scores <- data.frame("time"=scores$time[scores$team=="MSU"],"MSU_Score"=cumsum(scores$score[scores$team=="MSU"]), "UW_Score"=0)
 MSU_scores
-remove(game_scoring)
+plot(x=MSU_scores$time, y=MSU_scores$MSU_Score, type='l',xlab="time", ylab ="score", col="dark green")
+lines(x=UW_scores$time, y=UW_scores$UW_Score, col ="red")
+
