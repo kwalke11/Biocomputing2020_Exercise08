af14b34d5d0e7856238cbce6d1a58a3267a992ab
Kyle Walker <kwalke11@nd.edu>
2020-10-22 17:16:43 -0400

    Finished problem 1 with comments

--- Exercise08_Script.R
+++ Exercise08_Script.R
@@ -1,10 +1,9 @@
 setwd("~/Desktop/Biocomputing_R/Biocomputing2020_Exercise08") #set working directory
 scores <- read.table(file="UWvMSU_1-22-13.txt",header=TRUE, sep='') #read scores file and create table with scoring data
-cm_scores <- data.frame("time"=(scores$time),"team1"="UW", "team2"="MSU")
-UW_scores <- data.frame("time"=scores$time[scores$team=="UW"], "MSU_Score"=0,UW_Score=cumsum(scores$score[scores$team=="UW"]))
-UW_scores
-MSU_scores <- data.frame("time"=scores$time[scores$team=="MSU"],"MSU_Score"=cumsum(scores$score[scores$team=="MSU"]), "UW_Score"=0)
-MSU_scores
-plot(x=MSU_scores$time, y=MSU_scores$MSU_Score, type='l',xlab="time", ylab ="score", col="dark green")
-lines(x=UW_scores$time, y=UW_scores$UW_Score, col ="red")
-
+UW_scores <- data.frame("time"=scores$time[scores$team=="UW"], UW_Score=cumsum(scores$score[scores$team=="UW"])) #create a dataframe with cumulative scores for UW
+UW_scores #check to see if dataframe is correct
+MSU_scores <- data.frame("time"=scores$time[scores$team=="MSU"],"MSU_Score"=cumsum(scores$score[scores$team=="MSU"])) #create a dataframe with cumulative scores for MSU
+MSU_scores #check to ensure dataframe is correct
+plot(x=MSU_scores$time, y=MSU_scores$MSU_Score, type='l',xlab="time", ylab ="score", col="dark green") #create a plot with a dark green line of cumulative MSU scores, x axis=time and y axis=score
+lines(x=UW_scores$time, y=UW_scores$UW_Score, col ="red") #add a red line with UW cumulative scores to the same plot 
+# end of problem #1
