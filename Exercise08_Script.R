d821bb86b8ece5f41aec9bf26c65aeb676948526
Kyle Walker <kwalke11@nd.edu>
2020-10-22 20:53:30 -0400

    Problem 2 without descriptions

--- Exercise08_Script.R
+++ Exercise08_Script.R
@@ -7,3 +7,31 @@ MSU_scores #check to ensure dataframe is correct
 plot(x=MSU_scores$time, y=MSU_scores$MSU_Score, type='l',xlab="time", ylab ="score", col="dark green") #create a plot with a dark green line of cumulative MSU scores, x axis=time and y axis=score
 lines(x=UW_scores$time, y=UW_scores$UW_Score, col ="red") #add a red line with UW cumulative scores to the same plot 
 # end of problem #1
+# start of problem #2
+x <- sample(1:100, 1) #choose a random number
+cat("I am thinking of a number 1-100...") #start game with instructions
+guess = 0
+guess <- as.integer(readline(prompt="Enter a guess: "))
+correct_guess = FALSE
+attempts = 0
+
+while (correct_guess == FALSE){
+      if(attempts > 10){
+        print("You have entered too many guesses")
+        break
+      }else if (guess > x){
+        print("LOWER")
+        attempt = attempts + 1
+        guess <- as.integer(readline(prompt="Enter another guess: "))
+      }else if (guess < x){
+        print("HIGHER")
+        attempt = attempts + 1
+        guess <- as.integer(readline(prompt="Enter another guess: "))
+      }else{
+        print("Your guess was correct")
+        correct_guess = TRUE
+        break
+      }
+  }
+
+
